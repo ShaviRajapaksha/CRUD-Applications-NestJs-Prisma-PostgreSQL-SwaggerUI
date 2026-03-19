0c8ea404bda6929ec98dab94f8cb6c015500e5c1
IT20272654 <shavindurajapaksha@gmail.com>
2026-03-19 23:51:20 +0530

    feat: implement UserController with CRUD operations for user management

--- crud-app/src/user/user.controller.ts
+++ crud-app/src/user/user.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto.ts';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+@ApiTags('Users')
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post()
+    create(@Body() CreateUserDto: CreateUserDto) {
+        return this.userService.create(CreateUserDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(Number(id));
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.userService.update(Number(id), updateUserDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.userService.remove(Number(id));
+    }
+}

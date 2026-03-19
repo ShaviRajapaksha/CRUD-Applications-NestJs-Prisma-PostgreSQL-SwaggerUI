757e5efa350159a97d859850704b6e56b1b69c86
IT20272654 <shavindurajapaksha@gmail.com>
2026-03-19 21:55:03 +0530

    feat: update CreateUserDto to use definite assignment assertions and add UpdateUserDto extending it

--- crud-app/src/user/dto/create-user.dto.ts.ts
+++ crud-app/src/user/dto/create-user.dto.ts.ts
@@ -5,10 +5,10 @@ export class CreateUserDto {
 
     @ApiProperty({ example: "John Doe", description: "The name of the user" })
     @IsString()
-    name: string;
+    name!: string;
 
     @ApiProperty({ example: "john.doe@example.com", description: "The email of the user" })
     @IsEmail()
-    email: string;
+    email!: string;
 
 }

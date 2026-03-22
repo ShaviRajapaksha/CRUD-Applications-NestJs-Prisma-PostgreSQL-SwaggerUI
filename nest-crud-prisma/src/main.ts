740def1f3be85b3f530bc70cbb9ce5ac207b9c0c
IT20272654 <shavindurajapaksha@gmail.com>
2026-03-22 16:15:27 +0530

    feat: add main application bootstrap file with Swagger setup

---
+++ nest-crud-prisma/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Prisma CRUD')
+    .setDescription('Simple CRUD API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
